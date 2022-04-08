08c416f71f928bbd52eb94513dc532fa5b0edfa6
Jacob Shand <69992586+jacob-shand@users.noreply.github.com>
2022-04-08 13:32:06 +1000

    Update web.js
    display logs better

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Let the battle begin!');
+app.get("/", function (req, res) {
+  res.send("Let the battle begin!");
 });
 
-app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
+app.post("/", function (req, res) {
+  console.log(req.body.state);
+  // var currentPosition =
+  // console.log(req.body);
+  const moves = ["F", "T", "L", "R"];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
