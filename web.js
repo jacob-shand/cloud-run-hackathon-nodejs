692689e4abd054c4f5cc8a808e83a1690bd2c3ad
Jacob Shand <69992586+jacob-shand@users.noreply.github.com>
2022-04-08 13:59:15 +1000

    Update web.js
    move to top corner
    defend for your life

--- web.js
+++ web.js
@@ -9,11 +9,78 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  console.log(req.body.state);
+  console.log(
+    req.body.state["https://attitudepebbles-2kxpytz2oa-ts.a.run.app"]
+  );
+  currentPlayers = req.body;
+  currentX =
+    req.body.state["https://attitudepebbles-2kxpytz2oa-ts.a.run.app"].x;
+  currentY =
+    req.body.state["https://attitudepebbles-2kxpytz2oa-ts.a.run.app"].y;
+  currentDirection =
+    req.body.state["https://attitudepebbles-2kxpytz2oa-ts.a.run.app"].direction;
+
+  moveNeeded = true;
+  if (currentDirection != "W" && currentX != 0) {
+    moveNeeded = false;
+    move = faceLeft(currentDirection);
+  } else if (currentX != 0 && moveNeeded) {
+    moveNeeded = false;
+    move = "F";
+  } else if (currentDirection != "N" && currentY != 0 && moveNeeded) {
+    moveNeeded = false;
+    move = faceUp(currentDirection);
+  }
+  if (currentY != 0 && moveNeeded) {
+    moveNeeded = false;
+    move = "F";
+  }
+
+  if (currentdirection == "E" || currentDirection == "S") {
+    move = checkOtherPlayers(currentPlayers, currentDirection);
+  }
+
   // var currentPosition =
   // console.log(req.body);
-  const moves = ["F", "T", "L", "R"];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+
+  res.send(move);
 });
 
 app.listen(process.env.PORT || 8080);
+
+function faceLeft(currentDirection) {
+  switch (currentDirection) {
+    case "N":
+      return "L";
+    case "E":
+      return "L";
+    case "S":
+      return "R";
+  }
+}
+
+function faceUp(currentDirection) {
+  switch (currentDirection) {
+    case "E":
+      return "L";
+    case "W":
+      return "R";
+    case "S":
+      return "R";
+  }
+}
+
+function checkOtherPlayers(currentPlayers, currentDirection) {
+  for (player in currentPlayers) {
+    if (
+      (player.x < 3 && player.y == 0 && currentDirection == "E") ||
+      (player.y < 3 && player.x == 0 && currentDirection == "S")
+    ) {
+      return "T";
+    } else if (currentDirection == "E") {
+      return "R";
+    } else {
+      return "L";
+    }
+  }
+}
